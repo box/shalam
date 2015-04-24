a17563b59a8cbbad09d25afb815a0200ec43e213
Matt Basta <mbasta@box.com>
2015-04-24 12:31:48 -0700

    Fix for invalid relative paths being generated in certain cases
    This fixes an issue where image paths are sometimes wrong.

--- test/processor.js
+++ test/processor.js
@@ -87,6 +87,186 @@ describe('Processor', function() {
 
     });
 
+    describe('getSortedRulesets', function() {
+        var mockProcessor = {
+            rulesets: [],
+        };
+
+        function getFakeRuleset(start, end) {
+            return {
+                ruleset: {
+                    range: {
+                        range: [start, end],
+                    },
+                },
+            };
+        }
+
+        it('should return the rulesets sorted by end position', function() {
+            var first = getFakeRuleset(0, 10);
+            var second = getFakeRuleset(11, 20);
+            var third = getFakeRuleset(21, 30);
+
+            mockProcessor.rulesets.push(second);
+            mockProcessor.rulesets.push(first);
+            mockProcessor.rulesets.push(third);
+
+            var output = Processor.prototype.getSortedRulesets.call(mockProcessor);
+
+            assert.equal(output[0], first);
+            assert.equal(output[1], second);
+            assert.equal(output[2], third);
+
+        });
+
+    });
+
+    describe('guessFileIndentation', function() {
+        var mockRange = [];
+        var mockProcessor = {
+            data: 'foo {\n\tabc: def;\n}\n',
+            rulesets: [
+                {
+                    declaration: {
+                        range: {
+                            range: mockRange,
+                        },
+                    }
+                },
+            ],
+        };
+
+        it('should return four spaces by default', function() {
+            mockProcessor.data = 'foo {\n    abc: def;\n}\n';
+            mockRange[0] = 10;
+            mockRange[1] = 19;
+            var output = Processor.prototype.guessFileIndentation.call(mockProcessor);
+            assert.equal(output, '    ');
+        });
+
+        it('should return a tab when a tab is set', function() {
+            mockProcessor.data = 'foo {\n\tabc: def;\n}\n';
+            mockRange[0] = 7;
+            mockRange[1] = 16;
+            var output = Processor.prototype.guessFileIndentation.call(mockProcessor);
+            assert.equal(output, '\t');
+        });
+
+    });
+
+    describe('getSpriteRelativePath', function() {
+        it('should generate the correct relative path', function() {
+            var mockProcessor = {
+                path: '/opt/shalam/style.css',
+            };
+            assert.equal(
+                Processor.prototype.getSpriteRelativePath.call(mockProcessor, '/opt/shalam/sprite.png'),
+                'sprite.png'
+            );
+        });
+
+        it('should generate the correct relative path across directories', function() {
+            var mockProcessor = {
+                path: '/opt/shalam/static/css/style.css',
+            };
+            assert.equal(
+                Processor.prototype.getSpriteRelativePath.call(mockProcessor, '/opt/shalam/static/img/sprite.png'),
+                '../img/sprite.png'
+            );
+        });
+
+    });
+
+    describe('generateRulesetSpriteData', function() {
+
+        it('should return the expected ruleset for empty input', function() {
+            assert.equal(
+                Processor.prototype.generateRulesetSpriteData(
+                    {newRules: []},
+                    '    '
+                ),
+                '\n    /* shalam! */;\n    /* end shalam */'
+            );
+        });
+
+        it('should return the expected ruleset for non-empty input', function() {
+            assert.equal(
+                Processor.prototype.generateRulesetSpriteData(
+                    {newRules: [
+                        new crass.objects.Declaration(
+                            'foo',
+                            new crass.objects.Expression([
+                                [null, new crass.objects.Number(123)],
+                            ])
+                        )
+                    ]},
+                    '    '
+                ),
+                '\n    /* shalam! */;\n    foo: 123;\n    /* end shalam */'
+            );
+        });
+
+    });
+
+    describe('rewriteCSS', function() {
+
+        it('should inject Shalam data in all the right places', function() {
+            var mockCSS = 'foo {\n\
+                bar: 123px;\n\
+                -shalam-sprite: "foo";\n\
+            }\n\
+            zip {\n\
+                -shalam-sprite: "foo";\n\
+                bar: 123px;\n\
+            }\n\
+            dupe {\n\
+                -shalam-sprite: "foo";\n\
+                bar: 123px;\n\
+                -shalam-sprite: "foo";\n\
+            }';
+            var parsed = crass.parse(mockCSS);
+
+            var declarationIndices = [1, 0, 2]; // Indices of -shalam-sprite in the css above
+            var mockProcessor = {
+                data: mockCSS,
+                guessFileIndentation: function() {
+                    return '    ';
+                },
+                generateRulesetSpriteData: function() {
+                    // This represents the indentation of this file.
+                    return '\n                /* << shalam >> */';
+                },
+            };
+            var rulesets = parsed.content.map(function(ruleset, i) {
+                return {
+                    declaration: ruleset.content[declarationIndices[i]],
+                    ruleset: ruleset,
+                };
+            });
+
+            Processor.prototype.rewriteCSS.call(mockProcessor, rulesets);
+            var expectedOutput = 'foo {\n\
+                bar: 123px;\n\
+                -shalam-sprite: "foo";\n\
+                /* << shalam >> */\n\
+            }\n\
+            zip {\n\
+                -shalam-sprite: "foo";\n\
+                /* << shalam >> */\n\
+                bar: 123px;\n\
+            }\n\
+            dupe {\n\
+                -shalam-sprite: "foo";\n\
+                bar: 123px;\n\
+                -shalam-sprite: "foo";\n\
+                /* << shalam >> */\n\
+            }';
+            assert.equal(mockProcessor.data, expectedOutput);
+
+        });
+
+    });
+
 });
 
 describe('getSpriteDecl()', function() {
@@ -98,13 +278,13 @@ describe('getSpriteDecl()', function() {
 
     it('should reject declarations that do not have an initial string', function() {
         assert.throws(function() {
-            Processor.getSpriteDecl(getDeclaration('sprite: 123'));
+            Processor.getSpriteDecl(getDeclaration('-shalam-sprite: 123'));
         });
     });
 
     it('should accept bare strings', function() {
         assert.deepEqual(
-            Processor.getSpriteDecl(getDeclaration('sprite: "foo/bar.png"')),
+            Processor.getSpriteDecl(getDeclaration('-shalam-sprite: "foo/bar.png"')),
             {
                 path: 'foo/bar.png',
                 destWidth: -1,
@@ -117,31 +297,31 @@ describe('getSpriteDecl()', function() {
 
     it('should reject declarations that have non-function second expression elements', function() {
         assert.throws(function() {
-            Processor.getSpriteDecl(getDeclaration('sprite: "foo" 123'));
+            Processor.getSpriteDecl(getDeclaration('-shalam-sprite: "foo" 123'));
         });
     });
 
     it('should reject declarations that have more than two expression elements', function() {
         assert.throws(function() {
-            Processor.getSpriteDecl(getDeclaration('sprite: "foo" dest-size(10px 10px) 123'));
+            Processor.getSpriteDecl(getDeclaration('-shalam-sprite: "foo" dest-size(10px 10px) 123'));
         });
     });
 
     it('should reject declarations that do not use dest-size as the second expression element', function() {
         assert.throws(function() {
-            Processor.getSpriteDecl(getDeclaration('sprite: "foo" foo-size(10px 10px)'));
+            Processor.getSpriteDecl(getDeclaration('-shalam-sprite: "foo" foo-size(10px 10px)'));
         });
     });
 
     it('should reject declarations that have an invalid dest-size', function() {
         assert.throws(function() {
-            Processor.getSpriteDecl(getDeclaration('sprite: "foo" dest-size(10px)'));
+            Processor.getSpriteDecl(getDeclaration('-shalam-sprite: "foo" dest-size(10px)'));
         });
     });
 
     it('should accept sized strings', function() {
         assert.deepEqual(
-            Processor.getSpriteDecl(getDeclaration('sprite: "foo/bar.png" dest-size(12px 34px)')),
+            Processor.getSpriteDecl(getDeclaration('-shalam-sprite: "foo/bar.png" dest-size(12px 34px)')),
             {
                 path: 'foo/bar.png',
                 destWidth: 12,
@@ -149,36 +329,6 @@ describe('getSpriteDecl()', function() {
                 destX: null,
                 destY: null,
             }
-        );
-    });
-
-});
-
-describe('generateRulesetSpriteData()', function() {
-    it('should return the expected ruleset for empty input', function() {
-        assert.equal(
-            Processor.generateRulesetSpriteData(
-                {newRules: []},
-                '    '
-            ),
-            '\n    /* shalam! */;\n    /* end shalam */'
-        );
-    });
-
-    it('should return the expected ruleset for non-empty input', function() {
-        assert.equal(
-            Processor.generateRulesetSpriteData(
-                {newRules: [
-                    new crass.objects.Declaration(
-                        'foo',
-                        new crass.objects.Expression([
-                            [null, new crass.objects.Number(123)],
-                        ])
-                    )
-                ]},
-                '    '
-            ),
-            '\n    /* shalam! */;\n    foo: 123;\n    /* end shalam */'
         );
     });
 
